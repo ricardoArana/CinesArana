10811c7295a79263aa33cf10d1bdc3bfc0f0a403
ricardoArana <ricardo.romero@iesdonana.org>
2022-11-14 11:24:08 +0100

    closes #9, closes #10, closes #11, closes #12, closes #13, closes #14, closes #15, closes #16, closes #17, closes #18, closes #19, closes #20, closes #21, closes #22

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the "web" middleware group. Now create something great!a
 |
 */
 Route::get('/', [CineController::class, 'notuser'])
